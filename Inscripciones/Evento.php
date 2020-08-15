06b8a13e661f4827e23e042615b0dee74076b3d5
Brian Lozano <bdavidlozano@gmail.com>
2020-08-14 23:11:35 -0500

    Se cambio la forma de ver los eventos por las fechas

--- Inscripciones/Evento.php
+++ Inscripciones/Evento.php
@@ -46,7 +46,7 @@
 							$evento = $datos['evento'];
 							$fecha_actual = date("Y-m-d");
 
-							$sql = "SELECT * FROM eventos WHERE nombre = '$evento' AND fecha_evento >= '$fecha_actual'";
+							$sql = "SELECT * FROM eventos WHERE nombre = '$evento' AND fecha_evento <= '$fecha_actual'";
 							//echo '<option value="'.$sql.'">'.$sql.'</option>';
 							$resultado = $mysqli->query($sql);
 							$row = $resultado->num_rows;
@@ -89,26 +89,6 @@
 
 	<!-- Js File_________________________________ -->
 	
-	<script>
-	// Example starter JavaScript for disabling form submissions if there are invalid fields
-	(function() {
-	  'use strict';
-	  window.addEventListener('load', function() {
-		// Fetch all the forms we want to apply custom Bootstrap validation styles to
-		var forms = document.getElementsByClassName('needs-validation');
-		// Loop over them and prevent submission
-		var validation = Array.prototype.filter.call(forms, function(form) {
-		  form.addEventListener('submit', function(event) {
-			if (form.checkValidity() === false) {
-			  event.preventDefault();
-			  event.stopPropagation();
-			}
-			form.classList.add('was-validated');
-		  }, false);
-		});
-	  }, false);
-	})();
-	</script>
 
 	<!-- Borrar datos -->
 	<script type="text/javascript">
