bdbc7aec74c919d25284f43b6f3986c62b3e81d3
Brian Lozano <bdavidlozano@gmail.com>
2020-08-16 15:24:09 -0500

    se puso una validacion a los formularios de clubes que permite en mostrar una alerta de que si esta seguro de enviar

--- Inscripciones/Form_Clubes.php
+++ Inscripciones/Form_Clubes.php
@@ -243,6 +243,9 @@
 			var forms = document.getElementsByClassName('needs-validation');
 			var validation = Array.prototype.filter.call(forms, function(form) {
 			form.addEventListener('submit', function(event) {
+				if (!confirm('¿Esta Seguro Que Desea Continuar?')) {
+					event.preventDefault();
+				}
 				if (form.checkValidity() === false) {
 				event.preventDefault();
 				event.stopPropagation();
