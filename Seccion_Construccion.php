4ce64b289896d5e75a344c718e11c8f53d8ed590
Brian Lozano <bdavidlozano@gmail.com>
2020-08-17 12:14:38 -0500

    Se le cambio el nombre al archivo seccion en construccion porque tenia tildes

---
+++ Seccion_Construccion.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <!-- For Resposive Device -->
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <!-- Favicon -->
+    <link rel="icon" href="imagenes/favicon/Logo.ico" type="image/x-icon"/>
+    <link rel="apple-touch-icon" href="imagenes/favicon/Logo.ico" type="image/x-icon"/>
+    <title>Página no encontrada !</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" type="text/css" href="librerias/bootstrap/bootstrap.min.css">
+    <!-- page css -->
+    <link href="css/error-pages.css" rel="stylesheet">
+</head>
+
+<body class="skin-default-dark fixed-layout">
+    <!-- ============================================================== -->
+    <!-- Main wrapper - style you can find in pages.scss -->
+    <!-- ============================================================== -->
+    <section id="wrapper" class="error-page">
+		<center>
+        <div class="error-box">
+            <div class="error-body text-center">
+                <!--h1>404</h1>
+                <h3 class="text-uppercase">Página no encontrada !</h3-->
+				<h3 class="text-uppercase">Esta Sección estará funcionando muy pronto.</h3>
+                <!--p class="text-muted m-t-30 m-b-30">Parece que tenemos problemas al cargar la página solicitada</p--><br><br/><p></p><br><br/><p></p><br><br/><p></p>
+                <!--a href="Administrador" class="btn btn-info btn-rounded waves-effect waves-light m-b-40">Regresar a la página principal</a-->
+				<button class="btn btn-primary btn-rounded waves-effect waves-light m-b-40" onclick="history.back()">Regresar a la página anterior</button>
+			</div>
+        </div>
+		</center>
+    </section>
+    <!-- ============================================================== -->
+    <!-- End Wrapper -->
+    <!-- ============================================================== -->
+    <!-- ============================================================== -->
+    <!-- All Jquery -->
+    <!-- ============================================================== -->
+    <script src="librerias/jquery.min.js"></script>
+    <!-- Bootstrap tether Core JavaScript -->
+    <script src="librerias/bootstrap/popper.min.js"></script>
+    <script src="librerias/bootstrap/bootstrap.min.js"></script>
+    <!--Wave Effects -->
+    <script src="js/waves.js"></script>
+</body>
+
+</html>
