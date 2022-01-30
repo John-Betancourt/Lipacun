61923437191eaed5b7b1f0661e29e6be07bace6f
John Betancourt Hernández <47832570+John-Betancourt@users.noreply.github.com>
2022-01-30 09:57:15 -0500

    se corrigen unos meta del tipo de contenido y se agrega autocomplete al formulario login

--- scripts.php
+++ scripts.php
@@ -1,8 +1,7 @@
 	
 	<!-- For Resposive Device -->
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<meta http-equiv="x-ua-compatible" content="ie=edge">
 	<meta name="Description" content="developers: Brian Lozano, John Betancourt, Category: Plataforma Lipacun">
 
 	<!-- Favicon -->
